a6ae8d32a46e2758644c30b61ea6cdc84ac2a436
Gbariel Guerra <gabrielguerra190803@gmail.comm>
2024-04-25 20:01:14 -0300

    url update

--- sw.js
+++ sw.js
@@ -1,39 +1,51 @@
-const cacheName = "sw-page";
+const CACHE_NAME = 'cache';
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const PRECACHE_ASSETS = [
+         './index.html',
+         './index.css',
+          './android/android-launchericon-512-512.png',
+          './android/android-launchericon-192-192.png',
+          './android/android-launchericon-144-144.png',
+          './android/android-launchericon-96-96.png',
+          './android/android-launchericon-72-72.png',
+          './android/android-launchericon-48-48.png',
 
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll([
-          './index.html',
-          './index.css',
-          './assets/images/app-icon.png',
-          './assets/app-icons/android/android-launchericon-48-48.png',
-          './assets/app-icons/android/android-launchericon-72-72.png',
-          './assets/app-icons/android/android-launchericon-96-96.png',
-          './assets/app-icons/android/android-launchericon-144-144.png',
-          './assets/app-icons/android/android-launchericon-192-192.png',
-          './assets/app-icons/android/android-launchericon-512-512.png'
-          
-        ]);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
+]
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(error => {
-        console.error('Error in fetching:', error);
-      })
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // Verifica se a solicitação pertence à extensão do Chrome
+      if (event.request.url.startsWith('chrome-extension://')) {
+        return fetch(event.request);
+      }
+
+      return fetch(event.request).then(response => {
+        // Verifica se a resposta é válida
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // Clona a resposta para armazenamento em cache
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          // Verifica se a solicitação não pertence à extensão do Chrome antes de armazená-la em cache
+          if (!event.request.url.startsWith('chrome-extension://')) {
+            cache.put(event.request, responseToCache);
+          }
+        });
+
+        return response;
+      });
+    })
   );
 });
+
+
+
+
+
+
